ecf067de2eac0ed5b3099986f4e716a58751b514
Felipe Del Valle <felipesalah@hotmail.com>
2021-01-04 17:24:15 -0300

    scrolling bkg

--- main.c
+++ main.c
@@ -1,29 +1,17 @@
 #include <stdio.h>
 #include <gb/gb.h>
-#include "SmileSprite.c"
+#include "GroundMap.c"
+#include "SimpleBackground.c"
 
 void main() {
-    UINT8 currentSpriteIndex = 0;
-    set_sprite_data(0, 2, Smiler);
-    set_sprite_tile(0, 0);
-    move_sprite(0, 88, 78);
-    SHOW_SPRITES;
+    set_bkg_data(0, 7, backgroundtiles);
+    set_bkg_tiles(0, 0, 40, 18, background);
+
+    SHOW_BKG;
+    DISPLAY_ON;
 
     while(1) {
-        switch(joypad()) {
-            case J_LEFT:
-                scroll_sprite(0, -10, 0);
-                break;
-            case J_RIGHT:
-                scroll_sprite(0 , 10, 0);
-                break;
-            case J_UP:
-                scroll_sprite(0, 0, -10);
-                break;
-            case J_DOWN:
-                scroll_sprite(0, 0, 10);
-                break;
-        }
-        delay(10);
+        scroll_bkg(1, 0);
+        delay(100);
     }
 }
